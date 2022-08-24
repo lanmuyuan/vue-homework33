699cfe383acdb4581266bac3f17c59df4b74c98a
蓝慕远 <10488913+lan-muyuan@user.noreply.gitee.com>
2022-08-24 16:37:04 +0800

    first commit

---
+++ vue-homework2/加加减减/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index) in random" :key="index">{{item}}<button @click="del(index)">删除</button></li>
+    </ul>
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      random:[]
+    }
+  },
+  methods:{
+    add(){
+      this.random.push(Math.floor(Math.random()*20))
+    },
+    del(index){
+      this.random.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-homework2/帅哥美女走一走/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in persons " :key="item.id">{{ item.name }}</li>
+    </ul>
+    <button @click="change">走一走</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      persons: [
+        {
+          name: '帅哥',
+          id: '001',
+        },
+        {
+          name: '美女',
+          id: '002',
+        },
+        {
+          name: '程序员',
+          id: '003',
+        }]
+    }
+  },
+  methods: {
+    change(){
+      this.persons.push(this.persons.shift());
+    }
+  }
+}
+</script>
+
+<style>
+</style>
